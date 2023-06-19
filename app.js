0a813bb91efd45a484fdab19b5ec8af0027fa06e
onishitaisei <97043781+onishitaisei@users.noreply.github.com>
2023-06-19 21:23:37 +0900

    section 2.19

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'test',
+//     age: 26,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+// tupleはpushが許可されてしまう
+// person.role.push('admin');
+// let favoriteActivities: [string];
+// favoriteActivities = ['Camp'];
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase());
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'test',
     age: 26,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
-var favoriteActivities;
-favoriteActivities = ['Camp'];
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person.role === Role.ADMIN) {
+    console.log('管理者ユーザです');
 }
