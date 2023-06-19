6392dc2e6ac2c7b87d09f6580e79fe08c535b67b
onishitaisei <97043781+onishitaisei@users.noreply.github.com>
2023-06-19 21:05:23 +0900

    section 2.15

--- app.ts
+++ app.ts
@@ -1,15 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean , phrase: string) {
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
+const person = {
+    name: 'test',
+    age: 26
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
