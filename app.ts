43589c8663f982917e73f755725c3fef57968dbf
onishitaisei <97043781+onishitaisei@users.noreply.github.com>
2023-06-19 22:48:57 +0900

    section 2.29

--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+
+
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code};
+}
+
+generateError('エラーが発生しました', 500);
--- memo.md
+++ memo.md
@@ -5,4 +5,6 @@ https://zenn.dev/antez/articles/a9d9d12178b7b2
 
 ・undefined
 
+・never
+
 ・unknown 型のユースケース
