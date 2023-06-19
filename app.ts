dee0bafd910284e80698ba3220b275bac0770ab3
onishitaisei <97043781+onishitaisei@users.noreply.github.com>
2023-06-19 21:18:49 +0900

    section 2.18

--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'test',
     age: 26,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 }
+
+// tupleはpushが許可されてしまう
+// person.role.push('admin');
 
 let favoriteActivities: [string];
 
