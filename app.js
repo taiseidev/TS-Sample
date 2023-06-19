e4d3d092b4d153630fb6a439e59f92f9269dbdf0
onishitaisei <97043781+onishitaisei@users.noreply.github.com>
2023-06-19 22:14:25 +0900

    section 2.22

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result;
+    // }
     return result;
 }
-var combineAges = combine(30, 20);
+var combineAges = combine(30, 20, 'as-number');
 console.log(combineAges);
-var combineNames = combine('Max', 'Anna');
+var combinedStringAges = combine('30', '20', 'as-number');
+console.log(combinedStringAges);
+var combineNames = combine('Max', 'Anna', 'as-text');
 console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,18 +1,22 @@
 type inputType = number | string;
 
-function combine(input1: inputType, input2: inputType) {
+function combine(input1: inputType, input2: inputType, resultConversion: 'as-number' | 'as-text') {
     let result: inputType;
 
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
+
     return result;
 }
 
-const combineAges = combine(30, 20);
+const combineAges = combine(30, 20, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Max', 'Anna');
+const combinedStringAges = combine('30', '20', 'as-number');
+console.log(combinedStringAges);
+
+const combineNames = combine('Max', 'Anna', 'as-text');
 console.log(combineNames);
